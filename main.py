b2cb9fa96c40babc57957d4732108ae472690c7d
lihui01 <lihui01@b.360.cn>
2017-10-27 11:06:21 +0800

    restful-blueprint modify

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask import redirect
-from flask_restful import Api
-from test_case.main import test
+from test_case1.main import test1
+from test_case2.main import test2
 
 app = Flask(__name__)
-api = Api()
-api.init_app(test)
-app.register_blueprint(test)
+app.register_blueprint(test1)
+app.register_blueprint(test2)
 
 
-@app.route('/login')
-@app.route('/')
-def login():
-    return redirect('/test_case/welcome')
+@app.route('/login1')
+@app.route('/1')
+def login1():
+    return redirect('/test_case1/welcome')
+
+
+@app.route('/login2')
+@app.route('/2')
+def login2():
+    return redirect('/test_case2/welcome')
 
 
 if __name__ == '__main__':
---
+++ test_case2/main.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from flask_restful import Api, Resource
+
+test2 = Blueprint("test_case2", __name__, url_prefix='/test_case2')
+api = Api(test2)
+
+
+class TestWelcome(Resource):
+    def get(self):
+        return 'Welcome to my test2 restful-blueprint'
+
+api.add_resource(TestWelcome, '/welcome')
+
