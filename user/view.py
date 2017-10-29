ce6e21c0df8d4885202726191a0e58be6b8771be
lihui01 <lihui01@b.360.cn>
2017-10-29 14:23:20 +0800

    view

--- user/view.py
+++ user/view.py
@@ -8,7 +8,33 @@ api = Api(user_view)
 
 class LoginView(MyResource):
     def get(self):
-        return 'Welcome to my test2 restful-blueprint'
+        return  '''
+        <html>
+
+            <head>
+
+                <meta charset="utf-8" />
+
+                <title>Flask - Test</title>
+
+            </head>
+
+            <body>
+
+                <form action="/" method="post">
+                    姓名：
+                    <input type="text" name="username">
+                    <input type="text" name="pw">
+                    <input type="submit" value="Submit" />
+                </form>
+
+            </body>
+
+        </html>
+        '''
+
+    def post(self):
+        pass
 
 
 api.add_resource(LoginView, '/login')
