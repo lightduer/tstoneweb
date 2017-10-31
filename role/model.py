494c01dc05acdb29de18ec3fb113bbbd5bcac666
270861410@qq.com <a18155253>
2017-10-31 22:23:33 +0800

    url_resource

--- role/model.py
+++ role/model.py
@@ -6,10 +6,13 @@ class EndpointResource(db.Model):
     __tablename__ = 'endpoint_resource'
 
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(1024))
     endpoint = db.Column(db.String(1024))
     description = db.Column(db.String(120))
 
-    def __init__(self, endpoint, desc):
+    def __init__(self,key_id=None, uuid='', endpoint='', desc=''):
+        self.id = key_id
+        self.uuid = uuid
         self.endpoint = endpoint
         self.description = desc
 
@@ -37,11 +40,28 @@ class WebItemResource(db.Model):
 class Role(db.Model):
     __tablename__ = 'role'
     id = db.Column(db.Integer, primary_key=True)
-    role_type = db.Column(db.Integer, nullable=False)
-    role_name = db.Column(db.String(32), unique=True)
+    # role_type = db.Column(db.Integer, nullable=False)
+    role_name = db.Column(db.String(32))
     desc = db.Column(db.String(255))
 
-    def __init__(self, role_type, role_name, desc):
-        self.role_type = role_type
+    def __init__(self, role_name, desc):
         self.role_name = role_name
         self.desc = desc
+
+
+class RoleEndpoint(db.Model):
+    __tablename__ = 'role_endpoint'
+    id = db.Column(db.Integer, primary_key=True)
+    role_id = db.Column(db.Integer)
+    resource_id = db.Column(db.Integer)
+    method = db.Column(db.String(32))
+    desc = db.Column(db.String(255))
+
+    def __init__(self, key_id=None, role_id=0, resource_id=0, method='GET', desc=''):
+        self.id = key_id
+        self.role_id = role_id
+        self.resource_id = resource_id
+        self.method = method
+        self.desc = desc
+
+
